9c7a074c862501fe8f0847486e92bc8b4f03e962
Vinh-Thuyen <ntvthuyen@gmail.com>
2020-05-29 16:58:26 +0700

    update dotfile

--- fish/config.fish
+++ fish/config.fish
@@ -1,9 +1,3 @@
 set fish_greeting
-set PATH /usr/local/sbin:/usr/local/bin:/usr/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/home/lightkeima/development/flutter/bin:/home/lightkeima/.vimpkg/bin $PATH
-set PATH /home/lightkeima/development/flutter/bin $PATH
-set PATH ~/.local/lib/python3.7/site-packages $PATH
-set PATH ~/.local/lib/python3.7/site-packages $PATH
-set PATH /.pub-cache/bin $PATH
-set PATH /home/lightkeima/Project/Dices/ $PATH
-set PATH /home/lightkeima/.local/bin $PATH
-set PATH /home/lightkeima/ $PATH
+set -gx PATH /home/lightkeima/.local/bin /home/lightkeima/.config/node_modules/.bin $PATH
+
