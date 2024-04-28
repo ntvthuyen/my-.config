86a8cf8e3f1267d19081d9eaf69405ed584a5207
ntvthuyen <ntvthuyen@gmail.com>
2024-04-28 16:24:31 +0800

    new config

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -2,7 +2,7 @@
 
 # Add this script to your wm startup file.
 
-DIR="$HOME/.config/polybar/blocks"
+DIR="$HOME/.config/polybar/"
 
 # Terminate already running bar instances
 killall -q polybar
@@ -11,4 +11,10 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+if type "xrandr"; then
+   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do 
+     MONITOR=$m polybar -q main -c "$DIR"/config.ini &
+  done
+else
+  polybar -q main -c "$DIR"/config.ini &
+fi
